0b511eb092790817a8b9d0fc4af74a794fc9a518
cp4444 <cp44448@gmail.com>
2022-11-16 07:11:05 -0500

    Added product.dart to the lib folder on 16/11 Done with the AppBar

---
+++ lib/product.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Product());
+}
+
+class Product extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFFF1EDEC),
+        appBar: AppBar(
+          backgroundColor: Color(0xFFF1EDEC),
+          title: Text('Product'),
+          titleTextStyle: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 20),
+          centerTitle: true,
+          leading: GestureDetector(
+            onTap: (){},
+            child: Icon(Icons.arrow_back,color: Colors.black),
+          ),
+          actions: <Widget>[
+            Padding(padding: EdgeInsets.all(20.0),
+            child: GestureDetector(
+              onTap: (){},
+              child: Icon(Icons.monitor_heart,color: Colors.redAccent),
+            ),
+            ),
+
+          ],
+
+        ),
+      )
+    );
+  }
+  
+}
